66a83b0624a869198e5ac0c07aa0a1b391836773
Baltii25 <baltilopezfonta@gmail.com>
2025-06-04 15:44:46 -0300

    Modificacion bmi.c, muestra categoria correspondiente

--- bmi.c
+++ bmi.c
@@ -7,14 +7,30 @@ float Weight;
 float Height;
 float Result;
 
-printf("Introduce tu peso en kilogramos: ");
-scanf("%f", &Weight);
-printf("Introduce tu altura en metros: ");
-scanf("%f", &Height);
-Result = Weight/(Height*Height);
+	printf("Introduce tu peso en kilogramos: ");
+	scanf("%f", &Weight);
+	printf("Introduce tu altura en metros: ");
+	scanf("%f", &Height);
+	Result = Weight/(Height*Height);
 
-printf("Tu indice BMI es de %.2f\n\n", Result);
-printf("Tabla BMI: \n IMC	 	Nivel de peso\n");
-printf("Menos de 18,5	Bajo peso \n18,5 - 24,9	Normal \n25 - 29,9	Sobrepeso \n30 o más	Obesidad");
+	printf("Tu indice BMI es de %.2f\n\n", Result);
+	printf("Tabla BMI: \n IMC	 	Nivel de peso\n");
+	printf("Menos de 18,5	Bajo peso \n18,5 - 24,9	Normal \n25 - 29,9	Sobrepeso \n30 o más	Obesidad\n");
+	if(Result<18.5){
+		printf("Tu IMC indica bajo peso");
+	}
+	else{
+		if(Result<25) {
+			printf("Tu IMC es normal");
+		}
+		else{
+			if(Result <30){
+				printf("Tu IMC indica sobrepeso");
+			}
+			else{
+				printf("Tu IMC indica obesidad");
+			}
+		}
+	}
 return 0;
 }
